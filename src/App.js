a1317e69efc8597625ae125b78e8a8c752a8c332
Matt Reed <matthewreed02@gmail.com>
2023-03-11 00:41:20 -0700

    loading vectors kinda works

--- src/App.js
+++ src/App.js
@@ -2,21 +2,33 @@ import logo from './logo.svg';
 import './App.css';
 import { useEffect, useState } from 'react';
 import InputSection from './components/inputSection/InputSection';
+import { loadVectors } from './wordVectors/loadVectors';
 // import {word2vec} from "word2vec";
 
 function App() {
   const [input, setInput] = useState("");
+  const [vectors, setVectors] = useState(undefined);
 
   useEffect(() => {
-    // const result = loadModel("text.txt", () => {})
-  }, [input])
+    if (vectors === undefined) {
+      loadVectors().then(({size, dim, vectors}) => {
+        setVectors(vectors);
+      }).catch(error => {
+        console.log("Error loading vectors: " + error);
+      })
+    }
+  }, [vectors])
+
+  const getVecForWord = word => {
+    return (vectors && vectors[word]) || [0,0];
+  }
 
   return (
     <div className="App">
       <header className="App-header"></header>
       <div className="Main-container">
         <div className='Left-section'>
-          <InputSection setInput={setInput} poem={input}/>
+          <InputSection setInput={setInput} poem={input} getVecForWord={getVecForWord}/>
         </div>
         <div className='Right-section'>
           <InputSection setInput={setInput} poem={input}/>
--- src/components/inputSection/InputSection.js
+++ src/components/inputSection/InputSection.js
@@ -1,8 +1,16 @@
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
 import './InputSection.css';
 
 function InputSection(props) {
-  const {poem, setInput} = props;
+  const {poem, setInput, getVecForWord} = props;
+  const [poemVectors, setPoemVectors] = useState([]);
+
+  useEffect(() => {
+    const v = [];
+    poem.split(" ").forEach(word => v.push(getVecForWord(word)))
+    setPoemVectors(v);
+  }, [poem])
+
   return (
     <div className="Main-container">
       <div className="Vertical-container">
@@ -18,6 +26,7 @@ function InputSection(props) {
           Save
         </button>
         <p>{poem}</p>
+        <p>{poemVectors}</p>
       </div>
     </div>
   );
